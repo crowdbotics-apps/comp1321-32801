9b27c2f71c9322f7ea6350e392c2e702de81dfaa
Crowdbotics <team@crowdbotics.com>
2022-01-12 13:59:28 +0000

    Initial commit.

---
+++ comp1321_32801/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for comp1321_32801 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comp1321_32801.settings')
+
+application = get_wsgi_application()
